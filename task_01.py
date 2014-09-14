f552be33a5e2884888953a9ee1944db2373f964b
Lauren Hazel <lauren.hazel@spsmail.cuny.edu>
2014-09-14 19:20:06 -0400

    Here is a new one

---
+++ task_01.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+raw_input("How old is Stevie?")
+print 4
+
+QUESTION_01 = raw_input( 'How old is Stevie')
+print 4
+print QUESTION_01
+
+# part 2 of Task_01
+
+raw_input('How many days until Thanksgiving? ')
+print 75
+
+QUESTION_02 = int( raw_input( "How many days until Thanksgiving? "))
+print 75
---
+++ task_04.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print('file working') # checking file to make sure it is working correctly.
+raw_input("What day is it?")
+print 'Sunday'
+raw_input("What time is it?  ")
+print ('0406')
+
+x = 'Sat'
+x = 'Sun'
+x > 600
+
+x = 
+
+
+
